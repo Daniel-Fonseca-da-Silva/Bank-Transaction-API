67639c2e4cec3ae7ac2064ceaaae5937decc2830
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-07-19 17:43:52 +0100

    feat: added another missing values

---
+++ src/test/java/dafon/tech/bank_app/BankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dafon.tech.bank_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
