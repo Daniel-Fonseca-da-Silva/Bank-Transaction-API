6d7f5b3bfdd90eb03ec8610075883c025986d8eb
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-07-19 17:41:40 +0100

    feat: created data loader

---
+++ src/main/java/dafon/tech/bank_app/config/DataLoader.java
@@ -0,0 +1,24 @@
+package dafon.tech.bank_app.config;
+
+import dafon.tech.bank_app.entity.WalletType;
+import dafon.tech.bank_app.repository.WalletTypeRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class DataLoader implements CommandLineRunner {
+
+    private final WalletTypeRepository walletTypeRepository;
+
+    public DataLoader(WalletTypeRepository walletTypeRepository) {
+        this.walletTypeRepository = walletTypeRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Arrays.stream(WalletType.Enum.values())
+                .forEach(walletType -> walletTypeRepository.save(walletType.get()));
+    }
+}
